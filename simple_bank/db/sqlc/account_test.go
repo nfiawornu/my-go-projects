da0d08078f4c0d44cd81372e2e340bf007ab5e6e
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-07-16 06:23:00 +0000

    updates test_account to use util package for testing

--- simple_bank/db/sqlc/account_test.go
+++ simple_bank/db/sqlc/account_test.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/nfiawornu/my-go-projects/simple_bank/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
