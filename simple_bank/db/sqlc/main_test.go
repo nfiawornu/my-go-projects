c41da630350bb29360a90af43a3c466a3c0e5e9a
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-07-17 21:08:37 +0000

    updates main_test to use pgxpool instead of sql/db

--- simple_bank/db/sqlc/main_test.go
+++ simple_bank/db/sqlc/main_test.go
@@ -1,34 +1,3 @@
-// package db
-
-// import (
-// 	"database/sql"
-// 	"log"
-// 	"os"
-// 	"testing"
-
-// 	_ "github.com/lib/pq"
-// )
-
-// const (
-// 	dbDriver = "postgres"
-// 	dbSource = "postgresql://postgres_nobel:postgres_nobel@localhost:5432/simple_bank?sslmode=disable"
-// )
-
-// // var testQueries *Queries
-// // // var testDB *sql.DB
-
-// // func TestMain(m *testing.M) {
-// // 	conn, err := sql.Open(dbDriver, dbSource)
-// // 	if err != nil {
-// // 		log.Fatal("cannot connect to the DB:", err)
-// // 	}
-// // 	// code := m.Run()
-// // 	// // testDB.Close()
-// // 	testQueries = New(conn)
-// // 	os.Exit(m.Run())
-
-// // }
-
 package db
 
 import (
