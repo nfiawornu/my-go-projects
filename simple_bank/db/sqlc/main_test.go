abe7e5960f9086819f3392ecd077c89772b8cdf5
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-07-23 19:47:45 +0000

    updates main_test.go to use env instead of hard coded creds

--- simple_bank/db/sqlc/main_test.go
+++ simple_bank/db/sqlc/main_test.go
@@ -7,18 +7,21 @@ import (
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/nfiawornu/my-go-projects/simple_bank/util"
 )
-
-const dbSource = "postgresql://postgres_nobel:postgres_nobel@localhost:5432/simple_bank?sslmode=disable"
 
 var testQueries *Queries
 var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
 	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
 	// Use context to establish connection
-	testDB, err = pgxpool.New(context.Background(), dbSource)
+	testDB, err = pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the DB:", err)
 	}
