d554265e011095a38f9186500de232e77e5154a2
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-07-29 21:07:14 +0000

    updates transfer api to use transferTx interface instead of transfer query

--- simple_bank/api/transfer.go
+++ simple_bank/api/transfer.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,9 +10,10 @@ import (
 )
 
 type createTransferRequest struct {
-	FromAccountID int64 `json:"from_account_id" binding:"required"`
-	ToAccountID   int64 `json:"to_account_id" binding:"required"`
-	Amount        int64 `json:"amount" binding:"required"`
+	FromAccountID int64  `json:"from_account_id" binding:"required"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required"`
+	Amount        int64  `json:"amount" binding:"required"`
+	Currency      string `json: "currency" binding: "required,currency"`
 }
 
 func (server *Server) createTransfer(ctx *gin.Context) {
@@ -22,18 +24,44 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		return
 	}
 
-	arg := db.CreateTransferParams{
+	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+	if !server.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+
+	arg := db.TransferTxParams{
 		FromAccountID: req.FromAccountID,
 		ToAccountID:   req.ToAccountID,
 		Amount:        req.Amount,
 	}
 
-	transfer, err := server.store.CreateTransfer(ctx, arg)
+	result, err := server.store.TransferTx(ctx, arg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, transfer)
+	ctx.JSON(http.StatusOK, result)
+}
+
+func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := server.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+	return true
 }
 
 type getTransferRequest struct {
