6c41bebb62e42de415a1c8f10493b0d79ddf6195
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-08-05 18:12:13 +0000

    update: updates comment

--- simple_bank/token/payload.go
+++ simple_bank/token/payload.go
@@ -35,7 +35,7 @@ func NewPayload(username string, duration time.Duration) (*Payload, error) {
 	return payload, nil
 }
 
-// Valid checks if the otken payload is valid or not
+// Valid checks if the token payload is valid or not
 func (payload *Payload) Valid() error {
 	if time.Now().After(payload.ExpiredAt) {
 		return ErrExpiredToken
