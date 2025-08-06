4d68c36b4a6cfa157974ea052217282faa10ffdb
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-08-06 23:15:00 +0000

    update: refactors account api to auto create account for a user

--- simple_bank/api/account.go
+++ simple_bank/api/account.go
@@ -2,14 +2,15 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/nfiawornu/my-go-projects/simple_bank/db/sqlc"
+	"github.com/nfiawornu/my-go-projects/simple_bank/token"
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -21,8 +22,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
 	arg := db.CreateAccountParams{
-		Owner:    req.Owner,
+		Owner:    authPayload.Username,
 		Currency: req.Currency,
 		Balance:  0,
 	}
@@ -59,6 +61,11 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to the authenticated user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+	}
 	ctx.JSON(http.StatusOK, account)
 }
 
