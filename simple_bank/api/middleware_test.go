fbc2791bf78f785a5ac5466753bac0ecf435f82a
Nobel Fiawornu <fiawornunobel@gmail.com>
2025-08-10 15:13:44 +0000

    adds testing for auth middleware implementation

--- simple_bank/api/middleware_test.go
+++ simple_bank/api/middleware_test.go
@@ -73,7 +73,7 @@ func TestAuthMiddleware(t *testing.T) {
 		{
 			name: "ExpiredToken",
 			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
-				addAuthorization(t, request, tokenMaker, "", "user", -time.Minute)
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", -time.Minute)
 
 			},
 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
